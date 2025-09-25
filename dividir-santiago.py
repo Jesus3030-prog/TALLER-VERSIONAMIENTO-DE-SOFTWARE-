0563fc696ff85b9ecb92187e93e5ea2bcf7b653e
ssantiagoo222000-create <ssantiagoo222000@gmail.com>
2025-09-25 12:16:19 -0500

    Update dividir-santiago.py

--- dividir-santiago.py
+++ dividir-santiago.py
@@ -1,15 +1,20 @@
-def suma(a, b):
-    return a + b
-
-def resta(a, b):
-    return a - b
-
 def dividir(a, b):
     if b == 0:
         return "Error: división por cero"
     return a / b
 
+if __name__ == "_Calculadora_basica.py__":
+   def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def potencia(a, b):
+    return a ** b
+
 if __name__ == "__Calculadora_basica.py__":
     print("Suma: 2 + 3 =", suma(2, 3))
     print("Resta: 5 - 2 =", resta(5, 2))
+    print("Potencia: 2 ** 3 =", potencia(2, 3))
     print("Dividir: 10 / 2 =", dividir(10, 2))
