7a6d3e1c0f4989bd0bc5db919e3ff6785ba04adb
andresduvan54-blip <andresduvan54@gmail.com>
2025-09-25 11:25:33 -0500

    Update multiplicar-Duvan.py

--- multiplicar-Duvan.py
+++ multiplicar-Duvan.py
@@ -1,1 +1,13 @@
+def suma(a, b):
+    return a + b
 
+def resta(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+if _name_ == "_Calculadora_basica.py_":
+    print("Suma: 2 + 3 =", suma(2, 3))
+    print("Resta: 5 - 2 =", resta(5, 2))
+    print("Multiplicar: 4 * 5 =", multiplicar(4, 5))
