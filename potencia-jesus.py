d0e4029262f1e0c2d95e30159392613c14125ed9
Jesus3030-prog <thedibum@gmail.com>
2025-09-25 12:15:20 -0500

    Update potencia-jesus.py

--- potencia-jesus.py
+++ potencia-jesus.py
@@ -7,7 +7,7 @@ def resta(a, b):
 def potencia(a, b):
     return a ** b
 
-if __name__ == "__main__":
+if __name__ == "__Calculadora_basica.py_":
     print("Suma: 2 + 3 =", suma(2, 3))
     print("Resta: 5 - 2 =", resta(5, 2))
     print("Potencia: 2 ** 3 =", potencia(2, 3))
