110827a0f195c6af9702d921431ad8524915e659
Jesus3030-prog <thedibum@gmail.com>
2025-09-26 21:10:19 -0500

    corregido el bloque principal con __main__

--- Calculadora_basica.py
+++ Calculadora_basica.py
@@ -15,7 +15,7 @@ def dividir(a, b):
         return "Error: división por cero"
     return a / b
 
-if __name__ == "__Calculadora_basica.py__":
+if __name__ == "__main__":
     print("Suma: 2 + 3 =", suma(2, 3))
     print("Resta: 5 - 2 =", resta(5, 2))
     print("Potencia: 2 ** 3 =", potencia(2, 3))
